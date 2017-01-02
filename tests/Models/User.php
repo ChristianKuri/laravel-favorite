3a19942ff72666bfb4d301b0f1d5c6633e7f43fa
Christian Kuri <christian.kuri.martinez@gmail.com>
2017-01-02 05:19:19 +0000

    fix PHPUnitTest commands on User Model

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -12,4 +12,63 @@ class User extends Model implements AuthenticatableContract
 
     protected $table = 'users';
     protected $guarded = [];
+
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return 'token';
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param string $value
+     */
+    public function setRememberToken($value)
+    {
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'tokenName';
+    }
 }
