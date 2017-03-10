399aa7532c030f5163629a68f8eec9ec0b516358
Christian Kuri <christian.kuri.martinez@gmail.com>
2017-03-09 18:20:23 -0600

    Add publish database

--- src/FavoriteServiceProvider.php
+++ src/FavoriteServiceProvider.php
@@ -21,7 +21,9 @@ class FavoriteServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		$this->loadMigrationsFrom(__DIR__.'/../migrations');
+		$this->publishes([
+			__DIR__.'/../migrations/create_favorites_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_favorites_table.php'),
+        ], 'migrations');	
 	}
 
 	/**
